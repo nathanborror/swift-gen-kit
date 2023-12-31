96a837a0e45e15c5ec2d414db77e8577f97eb250
Nathan Borror <nathan@nathanborror.com>
2023-12-31 10:19:53 -0800

    add init

--- Sources/GenKit/Types/Model.swift
+++ Sources/GenKit/Types/Model.swift
@@ -3,4 +3,9 @@ import Foundation
 public struct Model: Codable, Identifiable {
     public var id: String
     public var owner: String
+    
+    public init(id: String, owner: String) {
+        self.id = id
+        self.owner = owner
+    }
 }
