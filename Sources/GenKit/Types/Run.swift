85984499d0ba4e1095e64f1df5bf50155ecfbf83
Nathan Borror <nathan@nathanborror.com>
2024-04-05 09:56:47 -0700

    run

---
+++ Sources/GenKit/Types/Run.swift
@@ -0,0 +1,24 @@
+import Foundation
+import SharedKit
+
+public struct Run: Codable, Identifiable {
+    public var id: String
+    public var status: Status
+    public var started: Date
+    public var ended: Date?
+    
+    public enum Status: Codable {
+        case paused
+        case generating
+        case cancelled
+        case failed
+        case finished
+    }
+    
+    public init(id: String = .id, status: Status = .paused, started: Date = .now, ended: Date? = nil) {
+        self.id = id
+        self.status = status
+        self.started = started
+        self.ended = ended
+    }
+}
