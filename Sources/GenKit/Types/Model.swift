9a56eabe88f2e17985044824f788558f498e5547
Nathan Borror <nathan@nathanborror.com>
2024-09-08 19:41:49 -0700

    optional name

--- Sources/GenKit/Types/Model.swift
+++ Sources/GenKit/Types/Model.swift
@@ -2,13 +2,15 @@ import Foundation
 
 public struct Model: Codable, Identifiable, Sendable {
     public var id: String
+    public var name: String?
     public var owner: String
     public var contextWindow: Int?
     public var maxOutput: Int?
     public var trainingCutoff: Date?
     
-    public init(id: String, owner: String, contextWindow: Int? = nil, maxOutput: Int? = nil, trainingCutoff: Date? = nil) {
+    public init(id: String, name: String? = nil, owner: String, contextWindow: Int? = nil, maxOutput: Int? = nil, trainingCutoff: Date? = nil) {
         self.id = id
+        self.name = name
         self.owner = owner
         self.contextWindow = contextWindow
         self.maxOutput = maxOutput
