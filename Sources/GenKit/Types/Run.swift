e4bea31ea53675faefb2f06b40d05b67218a1fb7
Nathan Borror <nathan@nathanborror.com>
2024-09-09 12:09:29 -0700

    making Run useful

--- Sources/GenKit/Types/Run.swift
+++ Sources/GenKit/Types/Run.swift
@@ -3,21 +3,13 @@ import SharedKit
 
 public struct Run: Codable, Identifiable, Sendable {
     public var id: String
-    public var status: Status
+    public var messages: [Message]
     public var started: Date
     public var ended: Date?
     
-    public enum Status: Codable, Sendable {
-        case paused
-        case generating
-        case cancelled
-        case failed
-        case finished
-    }
-    
-    public init(id: String = .id, status: Status = .paused, started: Date = .now, ended: Date? = nil) {
+    public init(id: String = .id, messages: [Message] = [], started: Date = .now, ended: Date? = nil) {
         self.id = id
-        self.status = status
+        self.messages = messages
         self.started = started
         self.ended = ended
     }
