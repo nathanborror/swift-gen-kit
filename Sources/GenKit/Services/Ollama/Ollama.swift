0e3d3b0a18f80504ddd0d80014e9546f91c70333
Nathan Borror <nathan@nathanborror.com>
2025-04-30 21:57:12 -0700

    simplify

--- Sources/GenKit/Services/Ollama/Ollama.swift
+++ Sources/GenKit/Services/Ollama/Ollama.swift
@@ -13,29 +13,31 @@ public actor OllamaService {
     }
 }
 
+// MARK: - Services
+
 extension OllamaService: ChatService {
     
     public func completion(_ request: ChatServiceRequest) async throws -> Message {
         let req = ChatRequest(
             model: request.model.id,
-            messages: request.messages.map { .init($0) },
-            tools: request.tools.map { .init($0) },
+            messages: encode(messages: request.messages),
+            tools: encode(request.tools),
             stream: false
         )
         let resp = try await client.chatCompletions(req)
-        return .init(resp)
+        return decode(resp)
     }
     
     public func completionStream(_ request: ChatServiceRequest, update: (Message) async throws -> Void) async throws {
         let req = ChatRequest(
             model: request.model.id,
-            messages: request.messages.map { .init($0) },
-            tools: request.tools.map { .init($0) },
+            messages: encode(messages: request.messages),
+            tools: encode(request.tools),
             stream: true
         )
         var message = Message(role: .assistant)
         for try await resp in try client.chatCompletionsStream(req) {
-            message.patch(resp)
+            patchMessage(&message, with: resp)
             try await update(message)
             
             // The connection hangs if we don't explicitly return when the stream has stopped.
@@ -59,6 +61,138 @@ extension OllamaService: ModelService {
     
     public func models() async throws -> [Model] {
         let resp = try await client.models()
-        return resp.models.map { .init($0) }
+        return resp.models.map { decode($0) }
+    }
+}
+
+// MARK: - Encoders
+
+extension OllamaService {
+
+    private func encode(messages: [Message]) -> [Ollama.Message] {
+        messages.map { message in
+            Ollama.Message(
+                role: encode(message.role),
+                content: encode(message.contents),
+                images: encode(message.contents)
+            )
+        }
+    }
+
+    private func encode(_ role: Message.Role) -> Ollama.Message.Role {
+        switch role {
+        case .system: .system
+        case .user: .user
+        case .assistant: .assistant
+        case .tool: .tool
+        }
+    }
+
+    private func encode(_ contents: [Message.Content]?) -> String {
+        contents?.compactMap {
+            switch $0 {
+            case .text(let text): text
+            case .json(let json): json.object
+            default: ""
+            }
+        }.joined() ?? ""
+    }
+
+    private func encode(_ contents: [Message.Content]?) -> [Data]? {
+        let images = contents?.compactMap {
+            switch $0 {
+            case .image(let image):
+                return try? Data(contentsOf: image.url)
+            default:
+                return nil
+            }
+        }
+        return images?.isEmpty == true ? nil : images
+    }
+
+    private func encode(_ tools: [Tool]) -> [Ollama.Tool] {
+        tools.map { tool in
+            Ollama.Tool(
+                type: "function",
+                function: .init(
+                    name: tool.function.name,
+                    description: tool.function.description,
+                    parameters: tool.function.parameters
+                )
+            )
+        }
+    }
+}
+
+// MARK: - Decoders
+
+extension OllamaService {
+
+    private func decode(_ resp: Ollama.ChatResponse) -> Message {
+        Message(
+            role: decode(resp.message?.role) ?? .assistant,
+            content: resp.message?.content,
+            toolCalls: resp.message?.tool_calls?.map { decode($0) },
+            finishReason: decode(resp.done)
+        )
+    }
+
+    private func decode(_ role: Ollama.Message.Role?) -> Message.Role? {
+        guard let role else { return nil }
+        switch role {
+        case .system: return .system
+        case .user: return .user
+        case .assistant: return .assistant
+        case .tool: return .tool
+        }
+    }
+
+    private func decode(_ toolCall: Ollama.ToolCall) -> ToolCall {
+        ToolCall(
+            function: .init(
+                name: toolCall.function.name,
+                arguments: toolCall.function.arguments
+            )
+        )
+    }
+
+    private func decode(_ done: Bool?) -> Message.FinishReason? {
+        guard let done, done else { return nil }
+        return .stop
+    }
+
+    private func decode(_ model: Ollama.ModelResponse) -> Model {
+        Model(
+            id: model.model,
+            family: model.details?.family,
+            name: model.name,
+            owner: "ollama",
+            contextWindow: nil,
+            maxOutput: nil,
+            trainingCutoff: nil
+        )
+    }
+
+    private func patchMessage(_ message: inout Message, with resp: Ollama.ChatResponse) {
+
+        // Patch message content
+        if case .text(let text) = message.contents?.last {
+            if let patched = GenKit.patch(string: text, with: resp.message?.content) {
+                message.contents = [.text(patched)]
+            }
+        } else if let text = resp.message?.content {
+            message.contents = [.text(text)]
+        }
+
+        // Patch message tool calls
+        if let toolCalls = resp.message?.tool_calls {
+            if message.toolCalls == nil {
+                message.toolCalls = []
+            }
+            message.toolCalls! += toolCalls.map { decode($0) }
+        }
+
+        message.finishReason = decode(resp.done)
+        message.modified = .now
     }
 }
